01bdb5ac4401ead68a5f783778f4e9e9a03be3e5
jabg97 <jaiver0.1@hotmail.com>
2024-08-25 21:56:13 -0500

    init vue project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,104 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">{{ info }}</h1>
+        <label for="name"></label>
+        <br />
+        <v-text-field v-model="name"></v-text-field>
+        <v-btn color="primary" elevation="4" raised x-large @click="saludar">
+          Saludar</v-btn
+        >
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "HelloWorld",
+
+  mounted() {
+    this.saludar();
+  },
+  methods: {
+    saludar: function () {
+      const user = this.name ? this.name : "Usuario";
+      axios
+        .get(`http://localhost:4000?name=${user}`)
+        .then((response) => {
+          this.info = response.data;
+          alert(`Hola ${user}!`);
+        })
+        .catch((error) => {
+          this.info = error;
+          console.log(error);
+        });
+    },
+  },
+  data: () => ({
+    info: "loading server",
+    name: "",
+    ecosystem: [
+      {
+        text: "vuetify-loader",
+        href: "https://github.com/vuetifyjs/vuetify-loader",
+      },
+      {
+        text: "github",
+        href: "https://github.com/vuetifyjs/vuetify",
+      },
+      {
+        text: "awesome-vuetify",
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
+    ],
+    importantLinks: [
+      {
+        text: "Documentation",
+        href: "https://vuetifyjs.com",
+      },
+      {
+        text: "Chat",
+        href: "https://community.vuetifyjs.com",
+      },
+      {
+        text: "Made with Vuetify",
+        href: "https://madewithvuejs.com/vuetify",
+      },
+      {
+        text: "Twitter",
+        href: "https://twitter.com/vuetifyjs",
+      },
+      {
+        text: "Articles",
+        href: "https://medium.com/vuetify",
+      },
+    ],
+    whatsNext: [
+      {
+        text: "Explore components",
+        href: "https://vuetifyjs.com/components/api-explorer",
+      },
+      {
+        text: "Select a layout",
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
+      },
+      {
+        text: "Frequently Asked Questions",
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
+};
+</script>
